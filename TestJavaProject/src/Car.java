5402eeca14b4da818da663d6577f7c1aa6e975cd
lethaljester <lethaljester@192.168.1.147>
2017-03-03 18:18:35 -0800

    Initial Commitment of The java Test File

---
+++ TestJavaProject/src/Car.java
@@ -0,0 +1,12 @@
+
+public class Car {
+
+	public Car() {
+		// TODO Auto-generated constructor stub
+		message = "I am a car";
+	}
+	private String message;
+	public String getMessage(){
+		return message;
+	}
+}
---
+++ TestJavaProject/src/Main.java
@@ -0,0 +1,13 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Car car = new Car();
+		
+		System.out.println("Hello World");
+		System.out.println(car.getMessage());
+		System.out.println("Hello Git");
+	}
+
+}
